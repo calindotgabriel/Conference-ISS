b270de6428a37d96fb78c94cfcbcd6beb4feede1
calin <calindotgabriel18@gmail.com>
2017-05-20 16:08:07 +0300

    Sign Up Form and Its State

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 
 class SignupForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value});
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
-            <div class="jumbotron">Hi!</div>
+            <form onSubmit={this.onSubmit}>
+                <h1>Join our community!</h1>
+
+                <div className="form-group">
+                    <label className="control-label">Username</label>
+                    <input type="text" name="username" className="form-control"
+                           value={this.state.username} onChange={this.onChange}/>
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Password</label>
+                    <input type="text" name="password" className="form-control"
+                           value={this.state.password} onChange={this.onChange}/>
+                </div>
+
+                <div className="form-group">
+                    <button className="btn btn-primary btn-lg">
+                        Sign up
+                    </button>
+                </div>
+            </form>
         );
     }
 }
